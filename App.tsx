b8d0e8de04b914f8a8d3d6bfc3a635f8660e422d
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-16 13:26:48 +0700

    video 6

--- App.tsx
+++ App.tsx
@@ -1,13 +1,45 @@
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
 
   //jsx
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name: {name} </Text>
+        <TextInput
+          multiline
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+        />
+      </View>
+
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age: {age} </Text>
+        <TextInput
+
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+          keyboardType='numeric'
+          maxLength={2}
+        />
+      </View>
+
       <Text style={{ fontSize: 40, fontWeight: "600" }}>
         count = {count}
       </Text>
