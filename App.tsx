1c6f28408f8eba0aa118acb111414a5f524330cf
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-16 10:42:28 +0700

    video 4

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  //jsx
   return (
     <View style={styles.container}>
-      <Text>Hello World with hoidanit!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>hoidanit1</Text>
+        <Text style={styles.parent}>
+          hoidanit2
+          <Text style={styles.child}>bla bla</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>Hello World 1</Text>
+      <Text>Hello World 2</Text>
     </View>
   );
 }
 
+//css in js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +26,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: "red", fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  parent: {
+    fontSize: 60,
+    color: "green",
+  },
+  child: {
+    fontSize: 30,
+    color: "pink",
+  }
 });
