192b6d4b88ea369185b8325fe8e711c6890bbcc0
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-16 11:24:45 +0700

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,40 +1,20 @@
-import { Text, View } from "react-native"
-import HomeScreen from "./components/review/home";
-import DetailScreen from "./components/review/detail";
-import AboutScreen from "./components/review/about";
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { OPENSANS_REGULAR } from "./utils/const";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import AppNavigation from "./components/navigation/app.navigation";
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-SplashScreen.preventAutoHideAsync();
-const App = () => {
-    const [loaded, error] = useFonts({
-        [OPENSANS_REGULAR]: require('./assets/fonts/OpenSans-Regular.ttf'),
-    });
-
-    useEffect(() => {
-        if (loaded || error) {
-            SplashScreen.hideAsync();
-        }
-    }, [loaded, error]);
-
-    if (!loaded && !error) {
-        return null;
-    }
-
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <NavigationContainer>
-                <AppNavigation />
-            </NavigationContainer>
-        </SafeAreaView>
-    )
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
