3cdac8b1b9f6ebfb06dcee45dd79867a33c56bd7
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-17 23:04:30 +0700

    video 5

--- App.tsx
+++ App.tsx
@@ -1,15 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState<String>("xuanhiep");
+  const [test, setTest] = useState({
+    name: 'xun',
+    age: 25
+  });
+  const [count, setCount] = useState<number>(0);
+
+  //jsx
   return (
-    <View style={styles.container}>
-      <Text>Hello world with xun hiep!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <Text style={styles.youtuber}>Xun Hiep!</Text> 
+      <TextInput style={{
+        borderColor: "green",
+        borderWidth: 1
+      }} />
+      <Text>Hello world!</Text> 
+      <Text style={{fontSize:40, fontWeight: "600"}} >
+        count = {count}
+      </Text>
+      <View>
+        <Button 
+        color={"red"}
+        title='Increase' onPress={() => setCount(count + 1)}
+        />
+      </View>
     </View>
   );
 }
 
+//css in js
+// Không có khái niệm CSS
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +41,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  youtuber : {
+    color: "brown", fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10
+  }
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+    };
 };
+//đây là file giúp cho bạn dịch code từ taiscript sang javascript
+//tự động chèn import React form 'react' cho các bạn
