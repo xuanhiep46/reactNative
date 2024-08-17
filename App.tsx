8f5ebf8aa3e07a863f1a923388d15ffa34be27a1
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-18 00:14:38 +0700

    video 6

--- App.tsx
+++ App.tsx
@@ -3,22 +3,43 @@ import { Button, TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  const [name, setName] = useState<String>("xuanhiep");
-  const [test, setTest] = useState({
-    name: 'xun',
-    age: 25
-  });
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("xuanhiep");
+  const [age, setAge] = useState<number>(0);
 
   //jsx
   return (
     <View style={styles.container}> 
-      <Text style={styles.youtuber}>Xun Hiep!</Text> 
-      <TextInput style={{
+    <View>
+      <Text style={{fontSize:40, fontWeight: "600"}}>Name: {name} </Text>
+      <TextInput 
+      multiline //Tự động mở rộng mỗi khi ô xuống dòng
+      autoCapitalize={'words'} //tự động viết hoa (kh quan trọng)
+      onChangeText={(value) => setName(value)} //Cập nhật phần Name cho thằng name phía trên
+      style={{ 
         borderColor: "green",
-        borderWidth: 1
-      }} />
-      <Text>Hello world!</Text> 
+        borderWidth: 1, 
+        width: 200,
+        padding: 15
+      }} 
+      />
+    </View>
+
+    <View>
+      <Text style={{fontSize:40, fontWeight: "600"}}>Age: {age} </Text>
+      <TextInput 
+      onChangeText={(value) => setAge(+value)} //Khi thêm dấu + vào phần setAge thì nó sẽ Convert kiểu string sang number
+      style={{ 
+        borderColor: "green",
+        borderWidth: 1, 
+        width: 200,
+        padding: 15
+      }} 
+      keyboardType='numeric' //kiểu bàn phím
+      maxLength={2}//giới hạn 2 chữ số
+      />
+    </View>
+      
       <Text style={{fontSize:40, fontWeight: "600"}} >
         count = {count}
       </Text>
