e98d0889dd43d5e9b1a62f3517f621f7dd5dab63
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-18 11:44:42 +0700

    video 7

--- App.tsx
+++ App.tsx
@@ -1,71 +1,56 @@
 import { useState } from 'react';
-import { Button, TextInput, StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput, StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
+  const [students, setStudents] = useState([
+    {id: 1, name: "xun1", age: 18},
+    {id: 2, name: "xun2", age: 19},
+    {id: 3, name: "xun3", age: 18},
+    {id: 4, name: "xun4", age: 17},
+    {id: 5, name: "xun5", age: 16},
+    {id: 6, name: "xun6", age: 15},
+    {id: 7, name: "xun7", age: 14},
+    {id: 8, name: "xun8", age: 13},
+    {id: 9, name: "xun9", age: 12},
+    {id: 10,name: "xun0", age: 11},
 
-  const [count, setCount] = useState<number>(0);
-  const [name, setName] = useState<string>("xuanhiep");
-  const [age, setAge] = useState<number>(0);
+  ]) 
 
   //jsx
   return (
     <View style={styles.container}> 
-    <View>
-      <Text style={{fontSize:40, fontWeight: "600"}}>Name: {name} </Text>
-      <TextInput 
-      multiline //Tự động mở rộng mỗi khi ô xuống dòng
-      autoCapitalize={'words'} //tự động viết hoa (kh quan trọng)
-      onChangeText={(value) => setName(value)} //Cập nhật phần Name cho thằng name phía trên
-      style={{ 
-        borderColor: "green",
-        borderWidth: 1, 
-        width: 200,
-        padding: 15
-      }} 
-      />
-    </View>
-
-    <View>
-      <Text style={{fontSize:40, fontWeight: "600"}}>Age: {age} </Text>
-      <TextInput 
-      onChangeText={(value) => setAge(+value)} //Khi thêm dấu + vào phần setAge thì nó sẽ Convert kiểu string sang number
-      style={{ 
-        borderColor: "green",
-        borderWidth: 1, 
-        width: 200,
-        padding: 15
-      }} 
-      keyboardType='numeric' //kiểu bàn phím
-      maxLength={2}//giới hạn 2 chữ số
-      />
-    </View>
-      
-      <Text style={{fontSize:40, fontWeight: "600"}} >
-        count = {count}
-      </Text>
-      <View>
-        <Button 
-        color={"red"}
-        title='Increase' onPress={() => setCount(count + 1)}
-        />
-      </View>
+    <Text style={{fontSize: 60}} >Hello world</Text>
+    {/* ScrollView - cho ứng dụng có thể kéo thả */}
+    <ScrollView>
+      {students.map(item => {
+        //dùng vòng lặp map: tạo ra vòng lặp mà không làm thay đổi giá trị của biến ban đầu
+        return (
+          <View key={item.id} style={styles.list}>
+            <Text>{item.name}</Text>
+          </View>
+        )
+      })}
+    </ScrollView>
     </View>
   );
 }
 
 //css in js
-// Không có khái niệm CSS
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 15, //horizontal là hàng ngang
+    paddingTop:50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  youtuber : {
-    color: "brown", fontSize: 60,
-    borderColor: "green",
-    borderWidth: 1,
-    padding: 10
+  list: {
+    padding: 20,
+    backgroundColor: "pink",
+    marginBottom: 20
   }
 });
+
+//đã coi tới 3:02:46
+
