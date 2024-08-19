31127fa1d788ca91193a8e6a545b47525f8c93b5
xuanhiep46 <xuanhiep0881888@gmail.com>
2024-08-19 10:48:17 +0700

    todo list 1

--- App.tsx
+++ App.tsx
@@ -2,70 +2,117 @@ import { useState } from 'react';
 import { FlatList, Button, TextInput, StyleSheet, Text, View, ScrollView } from 'react-native';
 //import { FlatList } from 'react-native-gesture-handler';
 
+interface ITodo {
+  id: number;
+  name: string;
+  //định nghĩa kiểu đối tượng cho nó
+}
 export default function App() {
-  const [students, setStudents] = useState([
-    {id: 1, name: "xun1", age: 18}, //key
-    {id: 2, name: "xun2", age: 19},
-    {id: 3, name: "xun3", age: 18},
-    {id: 4, name: "xun4", age: 17},
-    {id: 5, name: "xun5", age: 16},
-    {id: 6, name: "xun6", age: 15},
-    {id: 7, name: "xun7", age: 14},
-    {id: 8, name: "xun8", age: 13},
-    {id: 9, name: "xun9", age: 12},
-    {id: 10,name: "xun0", age: 11},
-  ]) 
+  const [todo, setTodo] = useState("");
+
+  const [listTodo, setListTodo] = useState<ITodo[]>([]) // '[]' nghĩa là mảng rỗng
+
+  //{id: 1, name: "watching xun"}
+
+  function randomInteger(min: number, max:number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } //hàm tạo id số random cho id
+
+  const handleAddTodo = () => {
+    if(!todo) return;
+    setListTodo([...listTodo, 
+      { id: randomInteger(2, 2000000), name: todo}
+    ]);
+      setTodo("") // để rỗng thì mỗi khi m nhập xong thì phần chữ trong input nó sẽ tự động biến mất
+  }//tạo function như vầy chỉ cần gọi lại là nó tự chạy được
 
   //jsx
   return (
-    <View style={styles.container}> 
-    <Text style={{fontSize: 60}} >Hello world</Text>
-    
-    <FlatList
-    data={students} //lấy dữ liệu ở students
-    //numColumns={2}//số cột
-    keyExtractor={item => item.id + ""} 
-    //Gắn id làm phần key cho phần flatlist
-    //phần này giúp cho code được tối ưu hơn
-    renderItem={({item}) => {
-      return (
-        <View style={styles.list}>
-          <Text>{item.name}</Text>
-        </View>
-      )
-    }}
-    //Sao cái flatlist này sao nó giống vòng lặp thế nhỉ =))
+  <View style={styles.container}> 
+    {/* Header */}
+    <Text style={styles.header} >To-do App</Text>
 
-    />
-    {/* <ScrollView>
-      {students.map(item => {
-        //dùng vòng lặp map: tạo ra vòng lặp mà không làm thay đổi giá trị của biến ban đầu
-        return (
-          <View key={item.id} style={styles.list}>
-            <Text>{item.name}</Text>
-          </View>
-        )
-      })}
-    </ScrollView> */}
+    {/* Form */}
+    <View style={styles.body}>
+      <TextInput 
+      value={todo} //kiểm soát giá trị cho nó
+      style = {styles.todoInput}
+      onChangeText={(value) => setTodo(value)} //in luôn giá trị mà bạn nhập vào
+      />
+      <Button 
+        title='Add todo'
+        onPress={handleAddTodo} //onPress: nhấn và nó sẽ ...
+      />
     </View>
+
+    {/* List to-do */}
+    <View style={styles.body}>
+      <FlatList
+        data={listTodo}
+        keyExtractor={item => item.id+ ""} //để ở đây để nó biết cần phải render như thế nào
+        renderItem={({item}) => {
+          return (
+            <Text style={styles.todoItem}>{item.name}</Text>
+          )
+        }}
+      />
+      {/* <Text>{JSON.stringify(listTodo)}</Text> : in ra phần listTodo*/}
+    </View>
+
+  </View>
   );
 }
 
 //css in js
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 15, //horizontal là hàng ngang
-    paddingTop:50,
+    paddingTop: 30,
     flex: 1,
     backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
+
+  header: {
+    backgroundColor: "orange",
+    paddingHorizontal: 20,
+    textAlign: "center",
+    fontSize:60,
+  },
+
+  form: {
+
+  },
+
+  todoInput: {
+    padding: 5,
+    margin:15,
+    borderBottomWidth:1,
+    borderBottomColor: "green",
+  },
+
+  list_to_do: {
+
+  },
+
+  todoItem : {
+    fontSize:20,
+    borderWidth: 1,
+    borderStyle: "dashed",
+    marginBottom: 20,
+    padding:10,
+  },
+
   list: {
     padding: 20,
     backgroundColor: "pink",
     marginBottom: 20,
     //marginHorizontal:30
+  },
+
+  body: {
+    paddingHorizontal:10,
+    marginBottom:20,
   }
 });
 
